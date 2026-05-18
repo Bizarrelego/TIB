3d27f54819c67b7e5e14bfb7354711f8ddeedd0e
Bizarrelego <107704360+Bizarrelego@users.noreply.github.com>
2026-05-17 20:23:38 -0400

    delete patches

--- patch_globalState.js
+++
@@ -1,3 +0,0 @@
-// Wait, looking at src/state/globalState.js, `module.exports = new GlobalStateManager();`
-// In src/main.js, `const globalState = require('./state/globalState');`
-// But main.js has `if (!global.State) { global.State = {}; }`
