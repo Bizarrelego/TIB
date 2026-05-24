f5043fb4bbd51a3b9b3fee3c8c829451ecd1e65d
Bizarrelego <107704360+Bizarrelego@users.noreply.github.com>
2026-05-24 12:58:52 +0000

    feat: implement GlobalStatePopulator for O(1) dictionary indexing

--- src/state/globalState.js
+++ src/state/globalState.js
@@ -1,3 +1,4 @@
+const globalStatePopulator = require('./GlobalStatePopulator');
 class GlobalStateManager {
     constructor() {
         this.heapCache = new Map();
@@ -97,99 +98,7 @@ class GlobalStateManager {
     }
 
     update() {
-        if (typeof Game === 'undefined') return;
-
-        // Clear per-tick O(1) collections before populating
-        this.rooms.clear();
-        this.creeps.clear();
-        this.structures.clear();
-        this.creepsByRoom.clear();
-        this.structuresByRoom.clear();
-        this.spawnsByRoom.clear();
-        this.sitesByRoom.clear();
-        this.creepLookup.clear();
-
-        if (Game.rooms) {
-            for (const roomName in Game.rooms) {
-                this.rooms.set(roomName, Game.rooms[roomName]);
-            }
-        }
-
-        if (Game.creeps) {
-            for (const creepName in Game.creeps) {
-                const creep = Game.creeps[creepName];
-                this.creeps.set(creepName, creep);
-                this.creepLookup.set(creepName, creep);
-
-                if (creep.pos && creep.pos.roomName) {
-                    const roomName = creep.pos.roomName;
-                    let roomCreeps = this.creepsByRoom.get(roomName);
-                    if (!roomCreeps) {
-                        roomCreeps = new Map();
-                        this.creepsByRoom.set(roomName, roomCreeps);
-                    }
-                    const role = creep.memory && creep.memory.role ? creep.memory.role : 'default';
-                    let roleCreeps = roomCreeps.get(role);
-                    if (!roleCreeps) {
-                        roleCreeps = [];
-                        roomCreeps.set(role, roleCreeps);
-                    }
-                    roleCreeps.push(creep);
-                }
-            }
-        }
-
-        if (Game.structures) {
-            for (const structId in Game.structures) {
-                const struct = Game.structures[structId];
-                this.structures.set(structId, struct);
-
-                if (struct.pos && struct.pos.roomName) {
-                    const roomName = struct.pos.roomName;
-                    let roomStructures = this.structuresByRoom.get(roomName);
-                    if (!roomStructures) {
-                        roomStructures = new Map();
-                        this.structuresByRoom.set(roomName, roomStructures);
-                    }
-                    let structsOfType = roomStructures.get(struct.structureType);
-                    if (!structsOfType) {
-                        structsOfType = new Map();
-                        roomStructures.set(struct.structureType, structsOfType);
-                    }
-                    structsOfType.set(structId, struct);
-                }
-            }
-        }
-
-        if (Game.spawns) {
-            for (const spawnName in Game.spawns) {
-                const spawn = Game.spawns[spawnName];
-                if (spawn.pos && spawn.pos.roomName) {
-                    const roomName = spawn.pos.roomName;
-                    let roomSpawns = this.spawnsByRoom.get(roomName);
-                    if (!roomSpawns) {
-                        roomSpawns = new Map();
-                        this.spawnsByRoom.set(roomName, roomSpawns);
-                    }
-                    roomSpawns.set(spawn.id || spawnName, spawn);
-                }
-            }
-        }
-
-        if (Game.constructionSites) {
-            for (const siteId in Game.constructionSites) {
-                const site = Game.constructionSites[siteId];
-                if (site.pos && site.pos.roomName) {
-                    const roomName = site.pos.roomName;
-                    let roomSites = this.sitesByRoom.get(roomName);
-                    if (!roomSites) {
-                        roomSites = new Map();
-                        this.sitesByRoom.set(roomName, roomSites);
-                    }
-                    roomSites.set(siteId, site);
-                }
-            }
-        }
+        globalStatePopulator.populate(this);
     }
 
     clear() {
