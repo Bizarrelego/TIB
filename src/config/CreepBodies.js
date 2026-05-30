ede435bad8e10eb1868c87e17868a0b61d29b174
Bizarrelego <107704360+Bizarrelego@users.noreply.github.com>
2026-05-30 16:43:39 -0400

    Update CreepBodies.js

--- src/config/CreepBodies.js
+++ src/config/CreepBodies.js
@@ -2,12 +2,12 @@
  * Hardcoded creep body configurations for different roles.
  */
 
-const MINER_BODY = [WORK, WORK, MOVE];
+const HARVESTER_BODY = [WORK, WORK, MOVE];
 const HAULER_BODY = [CARRY, CARRY, MOVE, MOVE];
 const UPGRADER_BODY = [WORK, CARRY, MOVE, MOVE];
 
 module.exports = {
-  MINER_BODY,
+  HARVESTER_BODY,
   HAULER_BODY,
   UPGRADER_BODY
 };
